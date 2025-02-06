db3c10f2126f2fc4ba513d41b7df379c1cc7e677
MrCr1p3r <egg0078@gmail.com>
2025-02-06 19:02:50 +0100

    Shared: Added Exchange and ExchangeTradingPairStatus enums

---
+++ src/Shared/SharedLibrary/Enums/Exchange.cs
@@ -0,0 +1,11 @@
+namespace SharedLibrary.Enums;
+
+/// <summary>
+/// Represents the different cryptocurrency exchanges.
+/// </summary>
+public enum Exchange
+{
+    Binance,
+    Mexc,
+    Bybit,
+}
---
+++ src/Shared/SharedLibrary/Enums/ExchangeTradingPairStatus.cs
@@ -0,0 +1,11 @@
+namespace SharedLibrary.Enums;
+
+/// <summary>
+/// Represents the status of a trading pair tradability on an exchange.
+/// </summary>
+public enum ExchangeTradingPairStatus
+{
+    Available,
+    CurrentlyUnavailable,
+    Unavailable,
+}
