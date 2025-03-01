8663946d3f451f59e987b48602f5cc272e48870b
MrCr1p3r <egg0078@gmail.com>
2025-03-01 17:23:00 +0100

    Shared: added testing extensions for logging and hybrid cache mocking

---
+++ src/Shared/SharedLibrary/Extensions/Testing/MoqHybridCacheExtensions.cs
@@ -0,0 +1,74 @@
+using Microsoft.Extensions.Caching.Hybrid;
+using Moq;
+using Moq.Language.Flow;
+
+namespace SharedLibrary.Extensions.Testing;
+
+public static class MoqHybridCacheExtensions
+{
+    public static IReturnsResult<HybridCache> SetupGetOrCreateAsync<TState, TResult>(
+        this Mock<HybridCache> mockCache,
+        string key,
+        TResult expectedValue
+    )
+    {
+        return mockCache
+            .Setup(m =>
+                m.GetOrCreateAsync(
+                    It.Is<string>(s => s == key),
+                    It.IsAny<TState>(),
+                    It.IsAny<Func<TState, CancellationToken, ValueTask<TResult>>>(),
+                    It.IsAny<HybridCacheEntryOptions>(),
+                    It.IsAny<IEnumerable<string>>(),
+                    It.IsAny<CancellationToken>()
+                )
+            )
+            .ReturnsAsync(expectedValue);
+    }
+
+    public static IReturnsResult<HybridCache> SetupGetOrCreateAsyncToExecuteFactory<
+        TState,
+        TResult
+    >(this Mock<HybridCache> mockCache, string key)
+    {
+        return mockCache
+            .Setup(m =>
+                m.GetOrCreateAsync(
+                    It.Is<string>(s => s == key),
+                    It.IsAny<TState>(),
+                    It.IsAny<Func<TState, CancellationToken, ValueTask<TResult>>>(),
+                    It.IsAny<HybridCacheEntryOptions>(),
+                    It.IsAny<IEnumerable<string>>(),
+                    It.IsAny<CancellationToken>()
+                )
+            )
+            .Returns<
+                string,
+                TState,
+                Func<TState, CancellationToken, ValueTask<TResult>>,
+                HybridCacheEntryOptions,
+                IEnumerable<string>,
+                CancellationToken
+            >(async (_, state, factory, _, _, ct) => await factory(state, ct));
+    }
+
+    public static void VerifyGetOrCreateAsyncCalled<TState, T>(
+        this Mock<HybridCache> mockCache,
+        string key,
+        Times times
+    )
+    {
+        mockCache.Verify(
+            m =>
+                m.GetOrCreateAsync(
+                    It.Is<string>(s => s == key),
+                    It.IsAny<TState>(),
+                    It.IsAny<Func<TState, CancellationToken, ValueTask<T>>>(),
+                    It.IsAny<HybridCacheEntryOptions>(),
+                    It.IsAny<IEnumerable<string>>(),
+                    It.IsAny<CancellationToken>()
+                ),
+            times
+        );
+    }
+}
