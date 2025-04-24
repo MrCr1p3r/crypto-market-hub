3ace2593518d9911068d27037456fa0590b338d7
MrCr1p3r <egg0078@gmail.com>
2025-04-24 17:58:45 +0200

    SVC_Coins: Introduce TradingPairsRepository with CRUD operations
    - Added TradingPairsRepository to manage trading pairs in the database, implementing methods for retrieving, inserting, replacing, and deleting trading pairs.
    - Included necessary package references for EFCore.BulkExtensions and FluentValidation.
    - Created unit tests for TradingPairsRepository to ensure functionality and correctness of CRUD operations.

---
+++ src/SVC_Microservices/SVC_Coins/Repositories/TradingPairsRepository.cs
@@ -0,0 +1,75 @@
+using System.Data;
+using EFCore.BulkExtensions;
+using Microsoft.EntityFrameworkCore;
+using SVC_Coins.Domain.Entities;
+using SVC_Coins.Domain.ValueObjects;
+using SVC_Coins.Infrastructure;
+using SVC_Coins.Repositories.Interfaces;
+
+namespace SVC_Coins.Repositories;
+
+/// <summary>
+/// Repository for managing trading pairs in the database.
+/// </summary>
+public class TradingPairsRepository(CoinsDbContext context) : ITradingPairsRepository
+{
+    private readonly CoinsDbContext _context = context;
+
+    /// <inheritdoc />
+    public async Task<IEnumerable<TradingPairsEntity>> GetTradingPairsByCoinIdPairs(
+        IEnumerable<TradingPairCoinIdsPair> pairs
+    )
+    {
+        var tradingPairs = pairs
+            .Select(pair => new TradingPairsEntity
+            {
+                IdCoinMain = pair.IdCoinMain,
+                IdCoinQuote = pair.IdCoinQuote,
+            })
+            .ToList();
+
+        // Add UseTempDB if needed
+        var bulkConfig = new BulkConfig
+        {
+            UpdateByProperties =
+            [
+                nameof(TradingPairsEntity.IdCoinMain),
+                nameof(TradingPairsEntity.IdCoinQuote),
+            ],
+        };
+
+        await _context.BulkReadAsync(tradingPairs, bulkConfig);
+
+        var foundTradingPairs = tradingPairs.Where(tp => tp.Id != 0).ToList();
+        return foundTradingPairs;
+    }
+
+    /// <inheritdoc />
+    public async Task<IEnumerable<TradingPairsEntity>> InsertTradingPairs(
+        IEnumerable<TradingPairsEntity> tradingPairs
+    )
+    {
+        await _context.TradingPairs.AddRangeAsync(tradingPairs);
+        await _context.SaveChangesAsync();
+
+        return tradingPairs;
+    }
+
+    /// <inheritdoc />
+    public async Task<IEnumerable<TradingPairsEntity>> ReplaceAllTradingPairs(
+        IEnumerable<TradingPairsEntity> tradingPairs
+    )
+    {
+        _context.TradingPairs.RemoveRange(_context.TradingPairs);
+        await _context.TradingPairs.AddRangeAsync(tradingPairs);
+        await _context.SaveChangesAsync();
+
+        return tradingPairs;
+    }
+
+    /// <inheritdoc />
+    public async Task DeleteTradingPairsForIdCoin(int idCoin) =>
+        await _context
+            .TradingPairs.Where(tp => tp.IdCoinMain == idCoin || tp.IdCoinQuote == idCoin)
+            .ExecuteDeleteAsync();
+}
